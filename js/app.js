cb505c3f098186657ab5187b1e9f82382cb414b8
John Quaranta <john@Johns-MacBook-Air.local>
2023-04-13 20:50:44 -0500

    completed intro page which can save movie preferences. Sends user to faceoff.html on button select with an error catch to send users to error.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation()
+
+//constructors for main page movie preference values
+const button = document.querySelector('button');
+const Genre = document.getElementById('Genre');
+const lowRating = document.getElementById('ratingLow');
+const highRating = document.getElementById('ratingHigh');
+const Actors = document.getElementById('Actors')
+const yearFrom = document.getElementById('yearFrom');
+const yearTo = document.getElementById('yearTo');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    //tries to access faceoff.html, if it catches an error it redirects to error.html
+    try {
+  
+      window.location.href = 'htmls/faceoff.html';
+    } catch (error) {
+      console.error('An error occurred:', error);
+      window.location.href = 'error.html';
+    }
+  window.location.href = 'htmls/error.html';
+});
