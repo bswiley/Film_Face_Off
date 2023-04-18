7b9aa14e9dd01bca2b24d61df8bdb2ab26faa26d
Francisjung17 <78046912+Francisjung17@users.noreply.github.com>
2023-04-17 19:27:51 -0500

    api changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-$(document).foundation()
+//$(document).foundation()
+
+//var omdbURL = "http://www.omdbapi.com/?i=tt3896198&apikey=2d8eaee1";
+//var tmdbURL = "https://api.themoviedb.org/3/movie/550?api_key=0369d0746be36bbf12f206aeb60eac4d";
+
+//var tmdbURL = "https://api.themoviedb.org/3/search/movie/latest?api_key=0369d0746be36bbf12f206aeb60eac4d&language=en-US"
+
+//HTTPS Links for API call
+var tmdbURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=0369d0746be36bbf12f206aeb60eac4d&language=en-US"
+
 
 
 //constructors for main page movie preference values
@@ -24,10 +33,31 @@ button.addEventListener('click', function(event) {
   window.location.href = 'htmls/error.html';
 });
 
-var apiURL = "http://www.omdbapi.com/?i=tt3896198&apikey=2d8eaee1";
+//Prints a list of viable genres to the console.
+//This method will not be in final version, only exists to test api calls
+function returnGenreList(){
+fetch(tmdbURL).then(function(response){
 
-fetch(apiURL)
-.then(function(response){
-    return response.json;
-})
+  return response.json();
 
+}).then(function (data) {
+    for(var i=0; i<data.genres.length; i++){
+      console.log(data.genres[i]);
+    }
+});
+}
+//Searches an Actor by name and pulls their information from the database.
+//Only pulls their 3 most popular movies, may not work for what we want?
+function searchActor(actorName){
+var actorSearch = " https://api.themoviedb.org/3/search/person?query="+actorName+"&api_key=0369d0746be36bbf12f206aeb60eac4d&language=en-US&page=1&include_adult=false";
+
+fetch(actorSearch).then(function(response){
+
+  return response.json();
+
+}).then(function (data) {
+    console.log(data);
+});
+}
+
+searchActor("Nicolas Cage");
